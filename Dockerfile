2bc7b0384e65cfc5ede7e31edc264b6da3694d18
kforkaran <karangupta199920@gmail.com>
2020-05-27 01:16:27 +0530

    output file added and typo in filename fixed

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.7.3-stretch
+MAINTAINER Karan Gupta <karangupta199920@gmail.com>
 
 # Create a working directory
 WORKDIR /src
@@ -8,6 +9,7 @@ COPY . app.py /src/
 
 # Install packages from requirements.txt
 RUN pip install --upgrade pip && pip install --trusted-host pypi.python.org -r requirements.txt
+# hadolint ignore=DL3013
 
 # Expose port 80
 EXPOSE 80
--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -62,9 +62,9 @@ def predict():
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
-    LOG.info(f"Output prediction: {prediction}")
-    return jsonify({'prediction': prediction})
+    output_prediction = jsonify({'prediction': prediction})
+    LOG.info("Output prediction: \n%s", output_prediction.json)
+    return output_prediction
 
 if __name__ == "__main__":
     # load pretrained model as clf
